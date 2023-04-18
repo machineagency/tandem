ea9d6a11945f87bee4036133f6bcf68844e1a38d
Jasper O'Leary <jhaazpr@gmail.com>
2023-04-18 16:10:53 -0700

    successful e2e

--- server.ts
+++ server.ts
@@ -1,7 +1,13 @@
 import fetch from 'node-fetch';
+import { Request as NFRequest, Response as NFResponse } from 'node-fetch';
 import * as express from 'express';
+import { Request, Response } from 'express';
+import * as cors from 'cors';
+
+const duetHostname = "192.168.1.2";
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 const doFetch = () => {
@@ -18,8 +24,38 @@ const doFetch = () => {
     });
 };
 
-app.get('/', (req, res) => {
-    doFetch().then(data => res.json(data));
+const forward = (originalReq: Request) => {
+    return new Promise<NFResponse>((resolve, reject) => {
+        let path = originalReq.url;
+        let url = 'http://' + duetHostname + path;
+        let method = originalReq.method;
+        fetch(url, { method })
+            .then((proxyResponse) => {
+                resolve(proxyResponse);
+            })
+            .catch((err) => {
+                console.error("Could not talk to the Duet.");
+                reject(err)
+            });
+    });
+};
+
+app.get('*', (req, res) => {
+    forward(req).then((proxyResponse) => {
+        proxyResponse.json()
+            .then((proxyResponseData) => {
+                res.json(proxyResponseData);
+            })
+            .catch((err) => {
+                console.error("Got a response from the Duet, but could not parse it as a JSON.");
+                console.error(err);
+                res.sendStatus(500);
+            });
+    }).catch((err) => {
+        console.error("Did not get a proper response from the Duet.");
+        console.error(err);
+        res.sendStatus(500);
+    });
 })
 
 app.listen(port, () => {
