199abcd265e7a21f2fcf4ea33c1b1fc6f4ac2d80
yu zhang <zzyoctopus@gmail.com>
2023-07-24 13:58:54 -0700

    outer and automation integrated

--- fusion360/notebook-bridge/notebook-bridge.py
+++ fusion360/notebook-bridge/notebook-bridge.py
@@ -10,6 +10,7 @@ import urllib.error
 import urllib.parse
 import urllib.request
 from .CreateUserParameter import *
+from .CreateOuter import *
 from .PropellerCAM import *
 from . ExportSVG import *
 from . PostProcess import *
@@ -118,10 +119,25 @@ class ThreadEventHandler(adsk.core.CustomEventHandler):
                 new_cam_setup = response_json.get('setup_cam')
                 new_generate_svg = response_json.get('generate_svg')
                 new_export_sbp = response_json.get('export_sbp')
+                new_create_outer = response_json.get('create_outer')
 
-                
+                    
 
                 if(new_status != 'standby'):
+                    if self.content.get('create_outer') != new_create_outer:
+                        self.content['create_outer'] = new_create_outer
+                        if new_create_outer:
+                            bottomface, topface = createOuter()
+                            innerBtmLoopEdgeCount = bottomface.loops.item(0).edges.count
+                            innerLoopEdgeCount = topface.loops.item(0).edges.count
+                            holeFaces = holeDrill(topface)
+
+                            self.content['bottomface'] = bottomface
+                            self.content['topface'] = topface
+                            self.content['innerBtmLoopEdgeCount'] = innerBtmLoopEdgeCount
+                            self.content['innerLoopEdgeCount'] = innerLoopEdgeCount
+                            self.content['holeFaces'] = holeFaces
+                            
                     # Check if 'create_param' is the same, if not, update and execute.
                     if self.content.get('create_param') != new_params:
                         self.content['create_param'] = new_params
@@ -136,15 +152,15 @@ class ThreadEventHandler(adsk.core.CustomEventHandler):
                         if new_cam_setup:
                             for setup in new_cam_setup:
                                 if setup == "alignmentJig":
-                                    cam.create_alignmentJig()
+                                    cam.create_alignmentJig(self.content['holeFaces'])
                                 elif setup == "foamSurface":
                                     cam.create_foam_surface()
                                 elif setup == "foamBore":
-                                    cam.create_foam_bore()
+                                    cam.create_foam_bore(self.content['holeFaces'])
                                 elif setup == "topCut":
-                                    cam.create_top_cut()
+                                    cam.create_top_cut(getLoopWithEdgesOnFace(innerLoopEdgeCount, topface))
                                 elif setup == "bottomCut":
-                                    cam.create_bottom_cut()
+                                    cam.create_bottom_cut(getLoopWithEdgesOnFace(innerBtmLoopEdgeCount, bottomface))
 
                     # Check if 'generate_svg' is the same, if not, update and execute.
                     if self.content.get('generate_svg') != new_generate_svg:
@@ -153,7 +169,7 @@ class ThreadEventHandler(adsk.core.CustomEventHandler):
                             exportSVG()
                             
                     if self.content.get('export_sbp') != new_export_sbp:
-                        self.content['new_export_sbp'] = new_export_sbp
+                        self.content['export_sbp'] = new_export_sbp
                         if new_export_sbp:
                             for setupName in new_export_sbp:
                                 exportSBPWithSetupNamed(setupName)
@@ -204,3 +220,7 @@ def stop(context):
         if ui:
             ui.messageBox('Failed:\n{}'.format(traceback.format_exc()))
 
+def getLoopWithEdgesOnFace(edgeCount, face):
+    for i in range(face.loops.count):
+        if(face.loops.item(i).edges.count == edgeCount):
+            return face.loops.item(i).edges
